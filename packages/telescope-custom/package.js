56ec6174f099ed78d5a7ff4aba7da754ba2f27c3
Thomas Griffin <thomas+git@artsquare.com>
2015-07-14 10:38:31 -0500

    update with magellan theme

--- packages/telescope-custom/package.js
+++ packages/telescope-custom/package.js
@@ -5,5 +5,12 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
+
+    api.use('telescope:core');
+    api.use('templating');
+
   api.addFiles('custom.css', 'client');
+  api.addFiles('banner.js', 'client');
+  api.addFiles('banner.html', 'client');
+
 });
